d688a23fc1edb4bac2d7b57d86e33602c60576d6
victorsilasd69 <victorsilvads69@gmail.com>
2025-04-04 20:55:12 -0300

    Criando Novo Projeto

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
-function Header() {
+function Header(props) {
     return (
         <header>
-            <h1>Header Component</h1>
+            <h1>{props.title}</h1>
         </header>
     );
 }
