889a41037273e85d4aa45b3050946eb86ba572e1
victorsilasd69 <victorsilvads69@gmail.com>
2025-04-11 20:53:16 -0300

    Criando Pagina de Caastro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Home from './Pages/Home';
+import Router from './router';
 
 function App() {
     return (
-      <Home />
+      <Router />
     )    
 }
 
--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,11 +1,49 @@
 
-import Header from "../Header";
+import { useEffect } from 'react';
+import Header from '../Header';
+import axios from 'axios';
+
 
 function Form() {
+    useEffect(() =>{
+        axios.get('http://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response=>{
+                console.log(response.data);
+            })
+    },[]);
     return (
         <div>
             <Header title="React Form" />
-            Seja Bem-vindo ao Novo Projeto!
+            <form>
+                <fieldset>
+                    <legend>
+                        <h2>Dados de Cadastro</h2>
+                    </legend>
+
+                    <div>
+                        <label>Nome:
+                            <input type="text" nome="txtNome" id="txtNome" />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>Idade:
+                            <input type="number" nome="txtIdade" id="txtIdade" />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>UF:
+                            <select name="cmbUF" id="cmbUF" >
+                                <option value="0">Selecione uma opção</option>
+                            </select>
+                        </label>
+                    </div>
+                    <input type="submit" value="Salvar" />
+                </fieldset>
+            </form>
+
+            {/*<Link to="/"> Acessar cadastro </Link>*/}
         </div>
     )
 }
